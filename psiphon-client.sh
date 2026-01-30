ab12eb6fa70391122971e5f6b881f2595445f014
NDEVXLK <ndevslk@gmail.com>
2026-01-30 06:29:51 +0530

    Update psiphon-client.sh

--- psiphon-client.sh
+++ psiphon-client.sh
@@ -1,3 +1,3 @@
-sudo wget -q -O /usr/local/bin/psiphon-client https://raw.githubusercontent.com/ndevxlk/psiphon-client/main/psiphon-client
+sudo wget -4 -q -O /usr/local/bin/psiphon-client https://raw.githubusercontent.com/ndevxlk/psiphon-client/main/psiphon-client
 sudo chmod +x /usr/local/bin/psiphon-client
 psiphon-client
